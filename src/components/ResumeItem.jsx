fc5aa7c10aee0e129640057b0db2df7f7b6faefe
UserSampath <nalakasampathsmp@gmail.com>
2023-07-18 19:51:10 +0530

    about part completed

--- src/components/ResumeItem.jsx
+++ src/components/ResumeItem.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-
+import parse from 'html-react-parser'
 const ResumeItem = ({ icon, year, title, desc }) => {
   return (
     <div className="resume__item">
       <div className="resume__icon">{icon}</div>
-          <span className="resume__date">{year }</span>
-          <h3 className="resume__subtitle">{ title}</h3>
-          <p className="resume_description">{ desc}</p>
+      <span className="resume__date">{year}</span>
+      <h3 className="resume__subtitle">{parse(title)}</h3>
+      <p className="resume_description">{desc}</p>
     </div>
   );
 };
--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -21,7 +21,7 @@ const About = () => {
               <Info />
             </ul>
             <a href={CV} download="" className="button">
-              Download CV{" "}
+              Download Cv{" "}
               <span className="button__icon">
                 <FaDownload />
               </span>
