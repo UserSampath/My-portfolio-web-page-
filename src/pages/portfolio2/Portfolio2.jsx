297201bec296ca79da69059f3b4d56a92960a2e1
UserSampath <nalakasampathsmp@gmail.com>
2024-05-10 23:40:20 +0530

    portfolio_card

--- src/pages/portfolio2/Portfolio2.jsx
+++ src/pages/portfolio2/Portfolio2.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
-
+import "./portfolio.css"
 const Portfolio2 = () => {
   return (
-    <div>Portfolio2</div>
-  )
+    <section className="portfolio section">
+      <h2 className="section__title">
+        My <span>Portfolio</span>
+      </h2>
+      <div className="portfolio_container">
+        <div style={{width:"100%"}}>
+                  <div className="portfolio_card">
+                      vs
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Portfolio2
