d3d66f55cb8caeb8be3121587ed24306b0d291ef
UserSampath <nalakasampathsmp@gmail.com>
2025-01-05 04:01:35 +0530

    test

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -18,7 +18,7 @@ const Home = () => {
       <div className="home__content">
         <div className="home__data">
           <h1 className="home__title">
-            <span>I'm Nalaka Sampath</span> <br /> Full Stack Developer
+            <span>I'm Nalaka Sampath</span> <br /> Software Engineer
           </h1>
           <p className="home__description">
             I am a passionate and motivated person, dedicated to exploring new
